b4e638d31da2111f3e2b78fdb63da1ce0eaea180
Shahram Pourmakhodmi <shahram@poushdigital.com>
2025-02-26 05:21:15 +0100

    Some cleanup after moving loop to visualizer

--- game_of_life.py
+++ game_of_life.py
@@ -1,21 +1,12 @@
-from time import sleep
-
 from grid import Grid
 from visualizer import GameVisualizer
 
+
 def main():
-    # Create a 20x20 grid
-    grid = Grid(20, 20)
-    visualizer = GameVisualizer()
-    # Run the simulation for 50 generations
-    while True:
-        running, paused, _ = visualizer.handle_events()
-        if not running:
-            break
-        if not paused:
-            visualizer.draw_grid(grid.grid)
-            grid.next_generation()
-            sleep(0.2)
+    # Create a 30x40 grid
+    grid = Grid(30, 40)
+    visualizer = GameVisualizer(grid=grid)
+    visualizer.loop()
 
 if __name__ == "__main__":
     main()
